143935f8d2e32b273766c599999dfdd1fa41c848
Jan Machala <jan.machala@email.cz>
2012-11-15 01:16:56 +0100

    Spring mvc is properly configured

---
+++ src/flickr/src/java/flickr/controller/IndexController.java
@@ -0,0 +1,14 @@
+package flickr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value="/index.htm")
+	public String index() {
+		return "index";
+	}
+
+}
